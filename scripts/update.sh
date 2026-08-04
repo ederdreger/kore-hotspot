ebf12a5aea32cab2a321f8969bf10a4168f89049
Spedynet Telecom <spedynet@spedynet.com.br>
2026-08-03 23:07:26 -0300

    fix: adotar UniFi atraves do MikroTik

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 APP_NAME="Kore-HotSpot"
-SCRIPT_VERSION="v0.2.48"
+SCRIPT_VERSION="v0.2.49"
 REPO_URL="${REPO_URL:-https://github.com/ederdreger/kore-hotspot.git}"
 REPO_SLUG="${REPO_SLUG:-ederdreger/kore-hotspot}"
 BRANCH="${BRANCH:-main}"
